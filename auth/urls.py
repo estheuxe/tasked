21e9d394be20dd5cfedcb2a94bdda85a6e30d390
estheuxe <siberian.w7y@gmail.com>
2019-09-18 12:12:16 +0700

    raw yandex auth

--- auth/urls.py
+++ auth/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('yandex/', views.gettoken, name='gettoken'),
 	path('', views.index, name='index')
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import webbrowser
+
+appId = '11630581252b45c8b3d7459720ed2af1'
+appPw = 'c200a37d97064c03ab55b1a1a5a28402'
+
+url = 'https://oauth.yandex.ru/authorize?response_type=token&client_id=' + appId
 
 def index(request):
+	webbrowser.open(url)
 	return HttpResponse("auth page")
+
+def gettoken(request):
+	currUrl = request.get_full_path()
+	return HttpResponse(currUrl)
